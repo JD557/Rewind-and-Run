b61b23caa72c1f66d5f01d95d0db0e783b5ecdb2
João Costa <jdpc557@gmail.com>
2021-01-31 11:59:55 +0000

    Prevent accidental scrolling when embed

--- js/input.js
+++ js/input.js
@@ -1,4 +1,7 @@
 document.addEventListener("keydown", function(event) {
+		if([32, 37, 38, 39, 40].indexOf(event.keyCode) > -1) {
+			event.preventDefault();
+		}
 		switch (event.keyCode) {
 			case 32:if (preview) {time=11;}break; // SPACE - Skip preview
 			case 37:mode=-1;modeImage.attr("src","img/rev.svg");break; // LEFT
